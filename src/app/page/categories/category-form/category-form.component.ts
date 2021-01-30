32520eacbf0e067b267e906005985f3af941b892
Mário N. Lima <mario_gnosis@yahoo.com.br>
2021-01-30 20:42:16 +0000

    adding base method to categoryForm

--- src/app/page/categories/category-form/category-form.component.ts
+++ src/app/page/categories/category-form/category-form.component.ts
@@ -1,15 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AfterContentChecked, Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Category } from '../shared/category.model';
+import { CategoryService } from '../shared/category.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.css']
 })
-export class CategoryFormComponent implements OnInit {
+export class CategoryFormComponent implements OnInit, AfterContentChecked {
 
-  constructor() { }
+  currentAction: string;
+  categoryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessage: string[] = null;
+  submitingForm =  false;
+  category: Category = new Category();
+
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    console.log("Cat", this.category);
+    this.setCurrentAction();
+    this.buildCategoryForm();
+    this.loadCategory();
   }
 
+  ngAfterContentChecked() {
+  this.setPageTitle();
+  }
+
+  private setCurrentAction() {
+    //verifica o seguimento da url ex: categories/new
+    if (this.route.snapshot.url[0].path == "new") {
+      this.currentAction = "new"
+    } else {
+      this.currentAction = "edit"
+    }
+    }
+
+    private buildCategoryForm() {
+      this.categoryForm = this.formBuilder.group({
+        id: [null],
+        name: [null, [Validators.required, Validators.minLength(2)]],
+        description: [null]
+      })
+    }
+
+    private loadCategory() {
+      if (this.currentAction == 'edit') {
+        this.route.paramMap.pipe(
+          switchMap( params => this.categoryService.getById(+params.get('id')))
+        ).subscribe(
+          (category) => {
+            this.category = category;
+            this.categoryForm.patchValue(category); // binds loaded category data to categoryForm
+          },
+          (error) => alert('ocorreu um erro no servidor, tente mais tarde.')
+        )
+      }
+    }
+
+    private setPageTitle() {
+      if (this.currentAction == 'new') {
+        this.pageTitle = 'Cadastro de nova categoria';
+      } else {
+        const categoryName = this.category.name || ''
+        this.pageTitle = 'Editando categoria: ' + categoryName;
+      }
+    }
 }
